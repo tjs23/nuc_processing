6c301e1b7ee998fb45c9c482405bd92c37575358
Kai Wohlfahrt <kjw53@cam.ac.uk>
2017-12-18 12:42:54 +0000

    Use context manager for input files
    Need ExitStack (Python3 only) for variable number of output files, so have
    marked that for future migration.

--- nuc_processing/splitFastqBarcodes.py
+++ nuc_processing/splitFastqBarcodes.py
@@ -27,61 +27,56 @@ def splitBarcodes(inputFile1, inputFile2, outputDirectory, barcodeLen=3):
   if not os.path.exists(outputDirectory):
     os.makedirs(outputDirectory)
 
-  fpin1 = open(inputFile1, 'rU')
-  fpin2 = open(inputFile2, 'rU')
-
-  barcodeCount = {}
-  barcodeFp1 = {}
-  barcodeFp2 = {}
-  mismatched = 0
-  matched = 0
-  Ncount1 = 0
-  Ncount2 = 0
-
-  lines1 = readNextFourLines(fpin1)
-  while lines1:
-    if lines1[0][0] != '@':
-      raise Exception('lines1[0] does not start with @: "%s"' % lines1[0].strip)
-    lines2 = readNextFourLines(fpin2)
-    if lines2[0][0] != '@':
-      raise Exception('lines2[0] does not start with @: "%s"' % lines2[0].strip)
-
-    if lines1[1][barcodeLen] == 'N':
-      Ncount1 += 1
-
-    if lines2[1][barcodeLen] == 'N':
-      Ncount2 += 1
-
-    barcode1 = lines1[1][:barcodeLen]
-    barcode2 = lines2[1][:barcodeLen]
-    if barcode1 != barcode2:
-      mismatched += 1
-    else:
-      matched += 1
-      if barcode1 not in barcodeFp1:
-        fileName1 = os.path.join(outputDirectory, '%s_%s.fq' % (os.path.basename(inputFile1)[:-3], barcode1))
-        fileName2 = os.path.join(outputDirectory, '%s_%s.fq' % (os.path.basename(inputFile2)[:-3], barcode2))
-        fpout1 = open(fileName1, 'w')
-        fpout2 = open(fileName2, 'w')
-        barcodeFp1[barcode1] = fpout1
-        barcodeFp2[barcode2] = fpout2
-        barcodeCount[barcode1] = 0
-
-      fpout1 = barcodeFp1[barcode1]
-      fpout2 = barcodeFp2[barcode2]
-      barcodeCount[barcode1] += 1
-
-      writeLines(fpout1, lines1, barcodeLen)
-      writeLines(fpout2, lines2, barcodeLen)
+  with open(inputFile1, 'rU') as fpin1, open(inputFile2, 'rU') as fpin2:
+    barcodeCount = {}
+    barcodeFp1 = {}
+    barcodeFp2 = {}
+    mismatched = 0
+    matched = 0
+    Ncount1 = 0
+    Ncount2 = 0
 
     lines1 = readNextFourLines(fpin1)
+    while lines1:
+      if lines1[0][0] != '@':
+        raise Exception('lines1[0] does not start with @: "%s"' % lines1[0].strip)
+      lines2 = readNextFourLines(fpin2)
+      if lines2[0][0] != '@':
+        raise Exception('lines2[0] does not start with @: "%s"' % lines2[0].strip)
 
-  fpin1.close()
-  fpin2.close()
+      if lines1[1][barcodeLen] == 'N':
+        Ncount1 += 1
 
+      if lines2[1][barcodeLen] == 'N':
+        Ncount2 += 1
+
+      barcode1 = lines1[1][:barcodeLen]
+      barcode2 = lines2[1][:barcodeLen]
+      if barcode1 != barcode2:
+        mismatched += 1
+      else:
+        matched += 1
+        if barcode1 not in barcodeFp1:
+          fileName1 = os.path.join(outputDirectory, '%s_%s.fq' % (os.path.basename(inputFile1)[:-3], barcode1))
+          fileName2 = os.path.join(outputDirectory, '%s_%s.fq' % (os.path.basename(inputFile2)[:-3], barcode2))
+          fpout1 = open(fileName1, 'w')
+          fpout2 = open(fileName2, 'w')
+          barcodeFp1[barcode1] = fpout1
+          barcodeFp2[barcode2] = fpout2
+          barcodeCount[barcode1] = 0
+
+        fpout1 = barcodeFp1[barcode1]
+        fpout2 = barcodeFp2[barcode2]
+        barcodeCount[barcode1] += 1
+
+        writeLines(fpout1, lines1, barcodeLen)
+        writeLines(fpout2, lines2, barcodeLen)
+
+      lines1 = readNextFourLines(fpin1)
+
+  # TODO: Can use ExitStack to manage files in Python3
   for fpout1 in barcodeFp1.values():
     fpout1.close()
-
   for fpout2 in barcodeFp2.values():
     fpout2.close()
 
