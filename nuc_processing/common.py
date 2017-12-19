70d44c137c953fc0fb6b60d3e5da85899014836a
Kai Wohlfahrt <kjw53@cam.ac.uk>
2017-12-19 15:35:36 +0000

    Shorten merge_file_names

--- nuc_processing/common.py
+++ nuc_processing/common.py
@@ -1,6 +1,11 @@
 import gzip
 import re
 import os
+try:
+  from itertools import zip_longest
+except ImportError:
+  # Python 2
+  from itertools import izip_longest as zip_longest
 
 
 def open_file_r(file_path, buffering=-1):
@@ -17,9 +22,7 @@ def strip_ext(name, ext):
 
 
 def merge_file_names(file_path1, file_path2, sep='_'):
-  FILENAME_SPLIT_PATT = re.compile('[_\.]')
-
-  # same dir, need non truncated name
+  SPLIT_PATT = re.compile('[_\.]')
 
   dir_name1, file_name1 = os.path.split(file_path1)
   dir_name2, file_name2 = os.path.split(file_path2)
@@ -35,31 +38,10 @@ def merge_file_names(file_path1, file_path2, sep='_'):
     msg = 'Attempt to merge file names with different file extensions'
     raise Exception(msg)
 
-  parts1 = FILENAME_SPLIT_PATT.split(file_root1)
-  parts2 = FILENAME_SPLIT_PATT.split(file_root2)
-  parts3 = []
+  parts = []
+  for a, b in zip_longest(*map(SPLIT_PATT.split, [file_root1, file_root2])):
+    parts.append(a)
+    if a != b:
+      parts.append(b)
 
-  n1 = len(parts1)
-  n2 = len(parts2)
-  n = max(n1, n2)
-
-  for i in range(n):
-
-    if (i < n1) and (i < n2):
-      a = parts1[i]
-      b = parts2[i]
-
-      parts3.append(a)
-      if a != b:
-        parts3.append(b)
-
-    elif i < n1:
-      parts3.append(parts1[i])
-    else:
-      parts3.append(parts2[i])
-
-  file_root3 = sep.join(parts3)
-
-  file_path3 = os.path.join(dir_name1, file_root3 + file_ext1)
-
-  return file_path3
+  return os.path.join(dir_name1, sep.join(parts) + file_ext1)
