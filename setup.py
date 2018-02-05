5c1e7022a1c7ca162ce3d6eaa80b3fdda65aea4f
Kai Wohlfahrt <kjw53@cam.ac.uk>
2018-02-05 16:23:49 +0000

    Auto-generate compressed files for split test

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+import sys
+
+compat_pkgs = []
+if sys.version_info.major < 3:
+    compat_pkgs.append("pathlib2")
 
 setup(
     name="Nuc Processing",
     version="0.0.1",
     description="A program to generate contacts from Hi-C data",
-    install_requires=['numpy', 'scipy'],
+    install_requires=['numpy', 'scipy'] + compat_pkgs,
     packages=['nuc_processing'],
     include_package_data=True,
     entry_points={'console_scripts': [
