742dee490677ce50bfd31bd899067501358c1c1e
tjs23 <tjs23@cam.ac.uk>
2019-09-10 17:15:27 +0100

    TJS: Rename directory

--- nuc_process_example.cmd
+++ nuc_process_example.cmd
@@ -1,1 +1,1 @@
-python NucProcess.py -o P2E8 -v -a -k -re1 MboI  -re2 AluI -s 50-5000 -n 12 -f /data/genome/mm10_chromosomes/*.fa -g /data/genome/mm10 -i /data/hi-c/SLX-11272/SLX-11272.NoIndex.000000000-AP1LB.s_1.r_?_GAG.fq
+./nuc_process  /data/hi-c/Cell1.r[12].fq.gz -o Cell1 -v -a -k -re1 MboI -re2 AluI -s 50-5000 -n 8 -f /data/genome/mm10_chromosomes/*.fa -g /data/genome/mm10 -cn /data/genome/mm10_chromo_names.tsv
